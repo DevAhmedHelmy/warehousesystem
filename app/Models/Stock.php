a31933a76177d7414295b7a4eded2412889c8d32
DevAhmedHelmy <helmyahmed248@gmail.com>
2020-05-20 00:39:12 +0200

    helmy

--- app/Models/Stock.php
+++ app/Models/Stock.php
@@ -10,6 +10,6 @@ class Stock extends Model
 {
     public function products()
     {
-        return $this->belongsToMany(Product::class, 'stock_products', 'stock_id', 'product_id');
+        return $this->belongsToMany(Product::class, 'stock_products', 'stock_id', 'product_id')->withPivot('first_balance', 'end_balance','additions','outgoing');
     }
 }
